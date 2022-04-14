ec180db47ad51bce6081a690bb4b6d3f6f643a14
Shri_pradhan0807 <shrikant.pradhan@globallogic.com>
2022-04-14 23:56:32 +0530

    Initial commit

---
+++ src/main/java/com/shritech/gms/GmsServicingMsApplication.java
@@ -0,0 +1,13 @@
+package com.shritech.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsServicingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsServicingMsApplication.class, args);
+	}
+
+}
